aaf9855b5972ee6c7ccc76a034eb4ce91161ae21
buoi2 <PMT201@PMT3046>
2026-03-13 09:30:20 +0700

    buoi1-1

--- kythuatlaptrinh/buoi1/buoi1_1.cpp
+++ kythuatlaptrinh/buoi1/buoi1_1.cpp
@@ -9,6 +9,20 @@ struct person {
 	string address;
 };
 
+void ViewPersonList(vector<person> p) {
+	if (p.size() == 0)
+		cout << "An empty list" << endl;
+	else{
+		for (int i = 0; i < p.size(); i++) {
+			cout << "Person " << (i + 1) << endl;
+			cout << "\t+ Id: << p[i].id" << endl;
+			cout << "\t+ Name: << p[i].name" << endl;
+			cout << "\t+ Age: << p[i].age" << endl;
+			cout << "\t+ Address: << p[i].address" << endl;
+			
+		}
+	}
+}
 
 int main() {
 	vector<person> list;
@@ -29,6 +43,7 @@ int main() {
 		switch (cmd)
 		{
 		case 1: {
+			ViewPersonList(list);
 			break;
 		}
 		case 2: {
