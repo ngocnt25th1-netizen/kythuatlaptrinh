cad456586d07b23d4ab2d5d1e9301c85415d7de2
buoi2 <PMT201@PMT3046>
2026-03-13 09:05:36 +0700

    buoi1_1

---
+++ kythuatlaptrinh/buoi1/buoi1_1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+struct person {
+	int id;
+	string name;
+	int age;
+	string address;
+};
+
+
+int main() {
+	vector<person> list;
+	do {
+		system("cls");
+		cout << "--- HUMAN RESOURCE -----" << endl;
+		cout << "1. View person list" << endl;
+		cout << "2. Add a person" << endl;
+		cout << "3. Remove a person" << endl;
+		cout << "4. Fing a person by name" << endl;
+		cout << "5. Export to file" << endl;
+		cout << "6. Import from file" << endl;
+		cout << "0. Exit" << endl;
+		cout << "------------------------" << endl;
+		cout << "Your comand: ";
+		int cmd;
+		cin >> cmd;
+		switch (cmd)
+		{
+		case 1: {
+			break;
+		}
+		case 2: {
+			break;
+		}
+		case 3: {
+			break;
+		}
+		case 4: {
+			break;
+		}
+		case 5: {
+			break;
+		}
+		case 6: {
+			break;
+		}
+		case 0: {
+			return 0;
+		}
+		default: 
+			cout << "Yuor command int't found. Try again..." << endl;
+			break;
+		}
+		cout << "Press Enter to continue...";
+		cin.ignore();
+		cin.get();
+	} while (true);
+	return 0;
+}
