a791944e7d61884e5fee0add5eb1b9474aabe047
buoi2 <PMT201@PMT3046>
2026-03-27 09:19:23 +0700

    buoi2

---
+++ kythuatlaptrinh/buoi2/buoi2.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+struct Person {
+	int id;
+	string name;
+};
+
+struct Node {
+	Person data;
+	Node* next;
+};
+
+struct LinkedList {
+	Node* head;
+};
+
+
+
+int main()
+{
+	LinkedList list;
+}
+
