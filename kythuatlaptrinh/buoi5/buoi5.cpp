2fc2c32e13b2532de89de5313451ce3294bbd258
PMT201 <PMT201@PMT3046>
2026-04-03 10:07:41 +0700

    buoi5

--- kythuatlaptrinh/buoi5/buoi5.cpp
+++ kythuatlaptrinh/buoi5/buoi5.cpp
@@ -30,5 +30,52 @@ struct LinkedList {
 int main()
 {
 	LinkedList books = { NULL };
+	do {
+		system("cls");
+		cout << "------ BOOK MANAGEMENT -------" << endl;
+		cout << "1. Show all books" << endl;
+		cout << "2. Add a book" << endl;
+		cout << "3. Delete a book" << endl;
+		cout << "4. Update a book" << endl;
+		cout << "5. Find book" << endl;
+		cout << "6. Export to file" << endl;
+		cout << "7. Import from file" << endl;
+		cout << "0. Exit" << endl;
+		cout << "----------------" << endl;
+		cout << "Enter your choice: " << endl;
+		int choice;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1: {
+			break;
+			}
+		case 2: {
+			break;
+		}
+		case 3: {
+			break;
+		}
+		case 4: {
+			break;
+		}
+		case 5: {
+			break;
+		}
+		case 6: {
+			break;
+		}
+		case 7: {
+			break;
+		}
+		case 0: {
+			return 0;
+		}
+		default: {
+			cout << "Invalid choice, try again" << endl;
+			break;
+		}
+		}
+	} while (true);
 }
 
